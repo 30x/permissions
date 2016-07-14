798ce92d0faec90862d6a9ebdc6e8ea8c1c0f44e
Martin Nally <mnally@apigee.com>
2016-07-14 13:00:58 -0700

    next

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 'use strict';
 var http = require('http');
 var Pool = require('pg').Pool;
-var url = require('url')
-var querystring = require('querystring')
+var url = require('url');
+var querystring = require('querystring');
+var httpPatterns = require('./http-patterns.js')
+var getPostBody = httpPatterns.getPostBody;
+var methodNotAllowed = httpPatterns.methodNotAllowed;
+var notFound = httpPatterns.notFound;
+var badRequest = httpPatterns.badRequest;
+var found = httpPatterns.found;
+var created = httpPatterns.created;
+var respond = httpPatterns.respond;
+var internalizeURL = httpPatterns.internalizeURL;
+var internalizeURLs = httpPatterns.internalizeURLs;
+var externalizeURLs = httpPatterns.externalizeURLs;
 
 var PROTOCOL = 'http';
 var config = {
@@ -34,13 +45,14 @@ function verifyPermissions(permissions) {
   }
 }
 
-var OPERATIONS = ['creators', 'readers', 'updaters', 'deleters'];
+var OPERATIONPROPERTIES = ['creators', 'readers', 'updaters', 'deleters'];
+var OPERATIONS = ['create', 'read', 'update', 'deleters'];
 
 function calculateSharedWith(permissions) {
   var result = {}
-  for (var i = 0; i < OPERATIONS.length; i++)
-    if (permissions.hasOwnProperty(OPERATIONS[i])) {
-      var users = permissions[OPERATIONS[i]];
+  for (var i = 0; i < OPERATIONPROPERTIES.length; i++)
+    if (permissions.hasOwnProperty(OPERATIONPROPERTIES[i])) {
+      var users = permissions[OPERATIONPROPERTIES[i]];
       for (var j = 0; j < users.length; j++) result[users[j]] = true;
     }
   permissions['_sharedWith'] = Object.keys(result)
@@ -91,9 +103,9 @@ function addAllowedActions(resource, user, result, callback) {
       if (pg_res.rowCount == 0) return 404;
       else {
         var row = pg_res.rows[0];
-        for (var i = 0; i < OPERATIONS.length; i++)
-          if (row.data.hasOwnProperty(OPERATIONS[i])) 
-            if (row.data[OPERATIONS[i]].indexOf(user) > -1) 
+        for (var i = 0; i < OPERATIONPROPERTIES.length; i++)
+          if (row.data.hasOwnProperty(OPERATIONPROPERTIES[i])) 
+            if (row.data[OPERATIONPROPERTIES[i]].indexOf(user) > -1) 
               result[OPERATIONS[i]] = true;
         callback()
       }
@@ -132,138 +144,6 @@ function requestHandler(req, res) {
     else notFound(req, res)
   }
 }
-
-//==============================================================================
-// Begin generic http functions that could be moved to a library
-
-function getPostBody(req, res, callback) {
-  var body = '';
-
-  req.on('data', function (data) {
-    if (body.length + data.length > 1e6)
-      req.connection.destroy();
-    body += data;
-  });
-  req.on('end', function () {
-    var jso;
-    try {
-      jso = JSON.parse(body);
-    }
-    catch (err) {
-      res.writeHead(400, {'Content-Type': 'text/plain'});
-      res.write('invalid JSON: ' + err.message);
-      res.end();          
-    }
-    if (jso) callback(req, res, jso);
-  });
-}
-
-function methodNotAllowed(req, res) {
-  var body = 'Method not allowed. request-target: ' + req.url + ' method: ' + req.method + '\n';
-  res.writeHead(405, {'Content-Type': 'text/plain',
-                      'Content-Length': Buffer.byteLength(body)});
-  res.end(err);
-}
-
-function notFound(req, res) {
-  var body = 'Not Found. request-target: ' + req.url + ' method: ' + req.method + '\n';
-  res.writeHead(404, {'Content-Type': 'text/plain',
-                      'Content-Length': Buffer.byteLength(body)});
-  res.end(body);
-}
-
-function badRequest(res, err) {
-  res.writeHead(400, {'Content-Type': 'text/plain',
-                      'Content-Length': Buffer.byteLength(err)});
-  res.end(err)
-}   
-
-function found(req, res, body, location, etag) {
-  var headers =  {}
-  if (location != null) headers['Content-Location'] = location; 
-  if (etag != null) headers['Etag'] = etag; 
-  respond(req, res, 200, headers, body)
-}
-
-function created(req, res, body, location, etag) {
-  var headers =  {}
-  if (location != null) headers['Location'] = location; 
-  if (etag != null) headers['Etag'] = etag; 
-  respond(req, res, 201, headers, body)
-}
-
-function respond(req, res, status, headers, body) {
-  if (body != null) {
-    headers['Content-Type'] = 'application/json';
-    body = JSON.stringify(body);
-    headers['Content-Length'] = Buffer.byteLength(body);
-    res.writeHead(status, headers);
-    res.end(body)
-  }
-  else { 
-    headers['Content-Length'] = 0;
-    res.writeHead(status, headers);
-    res.end(body)
-  }
-}
-
-function internalizeURL(anURL, authority) {
-  var httpString = 'http://' + authority;
-  var httpsString = 'https://' + authority;  
-  if (anURL.lastIndexOf(httpString) === 0) return anURL.substring(httpString.length);
-  else if (anURL.lastIndexOf(httpsString) === 0) return anURL.substring(httpsString.length);
-  else return anURL
-}
-
-function internalizeURLs(jsObject, authority) {
-  //strip the http://authority or https://authority from the front of any urls
-  if (typeof jsObject == 'object') {
-    var httpString = 'http://' + authority;
-    var httpsString = 'https://' + authority;
-    for(var key in jsObject) {
-      if (jsObject.hasOwnProperty(key)) {
-        var val = jsObject[key];
-        if (typeof val == 'string') {
-          if (val.lastIndexOf(httpString) === 0) jsObject[key] = val.substring(httpString.length);
-          else if (val.lastIndexOf(httpsString) === 0) jsObject[key] = val.substring(httpsString.length);
-        } else if (Array.isArray(val)) {
-          for (var i = 0; i < val.length; i++) {
-            var vali = val[i]
-            if (typeof vali == 'string') {
-              if (vali.lastIndexOf(httpString) === 0) val[i] = vali.substring(httpString.length);
-              else if (vali.lastIndexOf(httpsString) === 0) val[i] = vali.substring(httpsString.length);
-            } else internalizeURLs(vali, authority)             
-          }
-        } else internalizeURLs(val, authority)
-      }
-    }
-  }
-}  
-
-function externalizeURLs(jsObject, authority, protocol) {
-  //add http://authority or https://authority to the front of any urls
-  if (typeof jsObject == 'object') {
-    var prefix = protocol + '://' + authority;
-    for(var key in jsObject) {
-      if (jsObject.hasOwnProperty(key)) {
-        var val = jsObject[key];
-        if (typeof val == 'string') {
-          if (val.lastIndexOf('/') === 0) jsObject[key] = prefix + val;
-        } else if (Array.isArray(val)) {
-          for (var i = 0; i < val.length; i++) {
-            var vali = val[i]
-            if (typeof vali == 'string') 
-              if (vali.lastIndexOf('/') === 0) val[i] = prefix + val;
-            else internalizeURLs(vali, authority)             
-          }
-        } else internalizeURLs(val, authority)
-      }
-    }
-  }
-}  
-
-// End generic http functions that could be moved to a library
-//==============================================================================
 
 pool.query('CREATE TABLE IF NOT EXISTS permissions (subject text primary key, etag serial, data jsonb);', function(err, pg_res) {
   if(err) console.error('error creating permissions table', err);
