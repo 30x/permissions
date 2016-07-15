f5dbfef5292185bfe9f66263c91309bbecf6075b
Martin Nally <mnally@apigee.com>
2016-07-15 15:35:53 -0700

    lost files

---
+++ test/drop.js
@@ -0,0 +1,21 @@
+'use strict';
+var Pool = require('pg').Pool;
+
+var config = {
+  host: 'localhost',
+  user: 'martinnally',
+  password: 'martinnally',
+  database: 'permissions',
+};
+
+process.on('unhandledRejection', function(e) {
+  console.log(e.message, e.stack)
+})
+
+var pool = new Pool(config)
+
+pool.query('DROP TABLE IF EXISTS permissions', function(err, pg_res) {
+  if(err) console.error('error dropping permissions table', err);
+  else console.log('dropped table permissions')
+  pool.end()
+})
