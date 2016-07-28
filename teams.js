aa0a763b6db30f6fbc50e0cc66ea734b707092af
Martin Nally <mnally@apigee.com>
2016-07-28 13:22:00 -0700

    add comment

--- teams.js
+++ teams.js
@@ -1,4 +1,10 @@
 'use strict';
+/* 
+We dislike prerequisites and avoid them where possible. We especially dislike prereqs that have a 'framework' style; simple libraries are more palatable.
+The current code uses http and pg. Because of Node's callback style, these have a slightly 'frameworky' feel, but it is not practical to avoid these libraries.
+Please do not add any framework to this preqs. We do not want express or anything like it. We do not want any sort of "ORM" or similar.
+Adding simple library prereqs could be OK if the value they bring is in proportion to the problme being solved.
+*/
 var http = require('http');
 var Pool = require('pg').Pool;
 var url = require('url');
