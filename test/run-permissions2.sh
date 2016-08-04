8fd00ab0b0f19d840d6cd76263bd4d6298229d20
Martin Nally <mnally@apigee.com>
2016-08-04 13:39:57 -0700

    test multiple copies of permissions

---
+++ test/run-permissions2.sh
@@ -0,0 +1,10 @@
+export IPADDRESS="127.0.0.1"
+export PORT=3004
+export PG_HOST="localhost"
+export PG_USER="martinnally"
+export PG_PASSWORD="martinnally"
+export PG_DATABASE="permissions"
+export COMPONENT="permissions"
+
+node test/drop.js
+node permissions.js
