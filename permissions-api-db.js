4fdd8cd733a121dcb5a5858287d268ccfcf917ab
Martin Nally <mnally@apigee.com>
2016-08-05 21:07:45 -0700

    refactoring

--- permissions-api-db.js
+++ permissions-api-db.js
@@ -101,57 +101,15 @@ function createPermissionsThen(req, res, permissions, callback) {
 }
 
 function updatePermissionsThen(req, res, subject, patchedPermissions, etag, callback) {
-  // We use a transaction here, since its PG and we can. In fact it would be OK to create the event record first and then do the update.
-  // If the update failed we would have created an unnecessary event record, which is not ideal, but probably harmless.
-  // The converse—creating an update without an event record—could be harmful.
-  pool.connect(function(err, client, release) {
-    if (err) { 
-      lib.badRequest(res, err);
-    } else {
-      client.query('BEGIN', function(err) {
-        if(err) {
-          client.query('ROLLBACK', release);
-          lib.internalError(res, err);
-        } else {
-          lib.internalizeURLs(patchedPermissions, req.headers.host);
-          var key = lib.internalizeURL(subject, req.headers.host);
-          var query = 'UPDATE permissions SET data = ($1) WHERE subject = $2 AND etag = $3 RETURNING etag';
-          client.query(query, [patchedPermissions, key, etag], function(err, pgResult) {
-            if(err) {
-              client.query('ROLLBACK', release);
-              lib.internalError(res, err);
-            } else {
-              if (pgResult.rowCount === 0) {
-                client.query('ROLLBACK', release);
-                var resErr = 'If-Match header does not match stored etag ' + etag;
-                lib.badRequest(res, resErr);
-              } else {
-                var time = Date.now();
-                var query = `INSERT INTO events (topic, eventtime, data) 
-                             values ('permissions', ${time}, '{"subject": "${subject}", "action": "update", "etag": ${etag}}')
-                             RETURNING *`;
-                client.query(query, function(err, pgEventResult) {
-                  if(err) {
-                    client.query('ROLLBACK', release);
-                    lib.internalError(res, err);
-                  } else {
-                    if (pgEventResult.rowcount == 0) {
-                      client.query('ROLLBACK', release);
-                      lib.internalError(res, 'unable to create event');
-                    } else {
-                      client.query('COMMIT', release);
-                      callback(patchedPermissions, pgResult.rows[0].etag, pgEventResult.rows[0]);
-                      eventProducer.tellConsumers(req, pgEventResult.rows[0]);
-                    }
-                  }
-                });
-              }
-            }
-          });
-        }
-      });
-    }
-  });  
+  lib.internalizeURLs(patchedPermissions, req.headers.host);
+  var key = lib.internalizeURL(subject, req.headers.host);
+  var query = `UPDATE permissions SET data = ('${JSON.stringify(patchedPermissions)}') WHERE subject = '${key}' AND etag = ${etag} RETURNING etag`;
+  function eventData(pgResult) {
+    return {subject: patchedPermissions.governs._self, action: "create", etag: pgResult.rows[0].etag}
+  }
+  dblib.queryAndStoreEvent(req, res, pool, query, 'permissions', eventData, eventProducer, function(pgResult, pgEventResult) {
+    callback(patchedPermissions, pgResult.rows[0].etag);
+  });
 }
 
 function withResourcesSharedWithActorsDo(req, res, actors, callback) {
