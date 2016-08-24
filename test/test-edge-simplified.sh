6cf37d0708a504f7f5c848061f23fc3799c72597
Martin Nally <mnally@apigee.com>
2016-08-24 08:26:55 -0700

    update nginx.conf and change port from 8081 to 3000

--- test/test-edge-simplified.sh
+++ test/test-edge-simplified.sh
@@ -6,8 +6,8 @@ export PG_PASSWORD="martinnally"
 export PG_DATABASE="permissions"
 export COMPONENT="permissions"
 export SPEEDUP=10
-export EXTERNAL_ROUTER="localhost:8080"
-export INTERNAL_ROUTER="localhost:8080"
+export EXTERNAL_ROUTER="localhost:3000"
+export INTERNAL_ROUTER="localhost:8081"
 
 node drop.js
 python test-edge-simplified.py
