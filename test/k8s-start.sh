07cd5962b606de471b250d5b9ab0757925543388
Martin Nally <mnally@apigee.com>
2016-09-15 10:36:40 -0700

    bug in local k8s test script

--- test/k8s-start.sh
+++ test/k8s-start.sh
@@ -2,7 +2,7 @@ DIR=$(pwd)
 
 cd ~/perm/permissions
 
-source k8s-export-pg-connection-variables.sh
+source test/k8s-export-pg-connection-variables.sh
 
 kubectl delete secret permissions --namespace=apigee
 kubectl create secret generic permissions --from-literal=pguser=${PG_USER} --from-literal=pgpassword=${PG_PASSWORD} --from-literal=pghost=${PG_HOST} --from-literal=pgdatabase=${PG_DATABASE} --namespace=apigee
