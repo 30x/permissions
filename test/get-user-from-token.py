0d956119c40b13628be44b7cc4eef1725a8f3e8b
Martin Nally <mnally@apigee.com>
2016-10-16 08:03:42 -0700

    add get-user-from-token.py to test

---
+++ test/get-user-from-token.py
@@ -0,0 +1,16 @@
+import base64, sys, json
+
+def b64_decode(data):
+    missing_padding = (4 - len(data) % 4) % 4
+    if missing_padding:
+        data += b'='* missing_padding
+    return base64.decodestring(data)
+
+def main(token):
+    user_claims = json.loads(b64_decode(token.split('.')[1]))      
+    user = '%s#%s' % (user_claims['iss'], user_claims['sub'])
+    user_e = user.replace('#', '%23')
+    return user_e
+
+if __name__ == '__main__':
+    print main(sys.argv[1])
