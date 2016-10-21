149a402afdfcc3b11c594cbf4955e37e4521d5f8
Martin Nally <mnally@apigee.com>
2016-10-21 13:38:14 -0700

    separate router_host and router_port

--- test/e2e-test-edge-simplified.sh
+++ test/e2e-test-edge-simplified.sh
@@ -2,7 +2,7 @@ export IPADDRESS="127.0.0.1"
 export PORT=3004
 export COMPONENT="permissions"
 export SPEEDUP=10
-export EXTERNAL_ROUTER="shipyard.e2e.apigee.net"
+export EXTERNAL_ROUTER_HOST="shipyard.e2e.apigee.net"
 export EXTERNAL_SCHEME="https"
 
 source ../../aws-export-pg-variables.sh
