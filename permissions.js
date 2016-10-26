2168f85b7523d7812d43b5be0b9ed08f6eb1ec64
Martin Nally <mnally@apigee.com>
2016-10-26 10:31:40 -0700

    handle k8s hostIP on startup

--- permissions.js
+++ permissions.js
@@ -450,11 +450,25 @@ function requestHandler(req, res) {
   }
 }
 
-db.init(function () {
-  var port = process.env.PORT
-  permissionsEventConsumer.init(function() {
-    http.createServer(requestHandler).listen(port, function() {
-      console.log(`server is listening on ${port}`)
+function start() {
+  db.init(function () {
+    var port = process.env.PORT
+    permissionsEventConsumer.init(function() {
+      http.createServer(requestHandler).listen(port, function() {
+        console.log(`server is listening on ${port}`)
+      })
     })
   })
-})
+}
+
+if (process.env.INTERNAL_SY_ROUTER_HOST == 'kubernetes_host_ip') 
+  lib.getHostIPThen(function(err, hostIP){
+    if (err) 
+      process.exit(1)
+    else {
+      process.env.INTERNAL_SY_ROUTER_HOST = hostIP
+      start()
+    }
+  })
+else 
+  start()
