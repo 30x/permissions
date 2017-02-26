947e3a4b766518c6b510d7faa6b17e928449c34f
Martin Nally <mnally@google.com>
2017-02-26 08:36:27 -0800

    bump version

--- test/k8s-test-edge-simplified.sh
+++ test/k8s-test-edge-simplified.sh
@@ -11,5 +11,5 @@ export EXTERNAL_SCHEME="http"
 source local-export-pg-connection-variables.sh
 node delete-test-data.js
 echo "deleted test data"
-source renew-tokens.sh
+source ../../renew-tokens.sh
 python test-edge-simplified.py
