742afc69966e6f22c0570eca50e2a45260090334
Martin Nally <mnally@google.com>
2017-09-21 20:00:38 -0700

    provide token when deleting test data so that the running permissions runtime can accept the events
    Change-Id: Ibd8d213f8bda32c13910f7bb02d9b11291973a79

--- test/delete-test-data.js
+++ test/delete-test-data.js
@@ -1,6 +1,13 @@
 'use strict'
 var Pool = require('pg').Pool
 var pge = require('@apigee/pg-event-producer')
+// When deleting data, we need to provide a client token for the notifiction
+// to the permissions runtime. The http-helper-library provides helper functions
+// for getting a token.
+var lib = require('@apigee/http-helper-functions')
+// The response-helper-functions provides a helper function for wrapping
+// an error function with the interfce of an HTTP response object
+var rLib = require('@apigee/response-helper-functions')
 
 var config = {
   host: process.env.PG_HOST,
@@ -9,17 +16,29 @@ var config = {
   database: process.env.PG_DATABASE
 }
 
-console.log(`start delete test data: host: ${config.host} user: ${config.user} password: ${config.password} database: ${config.database}`)
+console.log(`start delete test data: host: ${config.host} database: ${config.database}`)
 var pool = new Pool(config)
 var eventProducer = new pge.eventProducer(pool)
 
 function deleteTestDataThen(eventTopic, table, callback) {
-  var query = `DELETE FROM ${table} WHERE data @> '{"test-data": true}'`
-  function eventData(pgResult) {
-    return {subject: null, action: 'deleteAll'}
-  }
-  eventProducer.queryAndStoreEvent({headers:{}}, query, [], eventTopic, eventData, function(pgResult, pgEventResult) {
-    callback()
+  // The following call wraps an error function in an object with the
+  // interface of Node's HTTP response object 
+  let errorHandler = rLib.errorHandler((err) => {
+    console.log('deleteTestDataThen - error', err)
+  })
+  lib.withValidClientToken(errorHandler, null, process.env.PERMISSIONS_CLIENTID, process.env.PERMISSIONS_CLIENTSECRET, process.env.AUTH_URL, newToken => {
+    var query = `DELETE FROM ${table} WHERE data @> '{"test-data": true}'`
+    function eventData(pgResult) {
+      return {subject: null, action: 'deleteAll'}
+    }
+    let req = {
+      headers: {
+        authorization: `Bearer ${newToken}`
+      }
+    }
+    eventProducer.queryAndStoreEvent(req, query, [], eventTopic, eventData, function(pgResult, pgEventResult) {
+      callback()
+    })
   })
 }
 
@@ -31,7 +50,13 @@ eventProducer.init(function(){
       if(err) console.error('error removing test data from teams table', err)
       else console.log(`removed all test data from teams table on ${process.env.PG_HOST}`)
       pool.end()
+      // The finalize method gives the eventProducer the opportunity to
+      // release any resources (e.g. timers) tht might prevent termination
       eventProducer.finalize()
+      // The finalize method gives the http-helper-functions library the 
+      // opportunity to release any resources (e.g. timers) tht might prevent 
+      // termination
+      lib.finalize()
     })
   })
 })
